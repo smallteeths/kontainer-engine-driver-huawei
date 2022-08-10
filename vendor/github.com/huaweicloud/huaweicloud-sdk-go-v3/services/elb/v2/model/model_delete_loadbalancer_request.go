5d8bc87bf5189d587f36280862e1d177907bae20
smallteeths <503630985@qq.com>
2022-08-10 18:51:01 +0800

    PANDARIA: Update vendor

---
+++ vendor/github.com/huaweicloud/huaweicloud-sdk-go-v3/services/elb/v2/model/model_delete_loadbalancer_request.go
@@ -0,0 +1,26 @@
+package model
+
+import (
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/core/utils"
+
+	"strings"
+)
+
+// Request Object
+type DeleteLoadbalancerRequest struct {
+
+	// 负载均衡器id
+	LoadbalancerId string `json:"loadbalancer_id"`
+
+	// （不再支持）级联删除负载均衡器
+	Cascade *bool `json:"cascade,omitempty"`
+}
+
+func (o DeleteLoadbalancerRequest) String() string {
+	data, err := utils.Marshal(o)
+	if err != nil {
+		return "DeleteLoadbalancerRequest struct{}"
+	}
+
+	return strings.Join([]string{"DeleteLoadbalancerRequest", string(data)}, " ")
+}
